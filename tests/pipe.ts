cc1817d1a35fd503bef33a53e7ed6abf4828317c
Alex Young <alex.young@hive.hr>
2017-12-23 23:25:35 +0000

    Add tests for pipeAsync

--- tests/pipe.ts
+++ tests/pipe.ts
@@ -1,4 +1,7 @@
-import { pipe } from "../lib";
+import { pipe, pipeAsync } from "../lib";
+
+const delay = <T>(x: T): Promise<T> =>
+    new Promise(r => setTimeout(() => r(x), 1));
 
 describe("pipe", () => {
     it("combines passed functions", () => {
@@ -10,6 +13,47 @@ describe("pipe", () => {
             x => x * 2,
             x => x - 1
         )(0);
+
+        expect(actual).toBe(expected);
+    });
+});
+
+describe("pipeAsync", () => {
+    it("combines async functions", async () => {
+        const expected = 5;
+
+        const actual = await pipeAsync<number, number>(
+            x => delay(x + 1),
+            x => delay(x + 2),
+            x => delay(x * 2),
+            x => delay(x - 1)
+        )(0);
+
+        expect(actual).toBe(expected);
+    });
+
+    it("combines mixed async and sync functions", async () => {
+        const expected = 5;
+
+        const actual = await pipeAsync<number, number>(
+            x => x + 1,
+            x => delay(x + 2),
+            x => delay(x * 2),
+            x => delay(x - 1)
+        )(0);
+
+        expect(actual).toBe(expected);
+    });
+
+    it("resolves a promise argument", async () => {
+        const expected = 5;
+
+        const actual = await pipeAsync<number, number>(
+            x => x + 1,
+            x => delay(x + 2),
+            x => delay(x * 2),
+            x => delay(x - 1)
+        )(delay(0));
 
         expect(actual).toBe(expected);
     });
