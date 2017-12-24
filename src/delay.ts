570175951552aeab67ee1e4738ca038e24cfa97a
Alex Young <alex.young@hive.hr>
2017-12-24 12:03:58 +0000

    Refactor to point-free style

--- src/delay.ts
+++ src/delay.ts
@@ -1,7 +1,10 @@
 import { curry2 } from "./curry";
 import { flip } from "./flip";
+import { pipe } from "./pipe";
 import { promisify } from "./promisify";
 
 export type Delay = (ms: number) => Promise<void>;
 
-export const delay: Delay = promisify(curry2(flip(setTimeout)));
+export const delay = pipe<typeof setTimeout, Delay>(flip, promisify)(
+    setTimeout
+);
